8c97da8cc8c9a525ae200a85b5c5d30483a41e03
Ismael <ismaelazd@hotmail.com>
2019-12-10 22:54:32 +0100

    fin exo5

--- src/js/ex5.js
+++ src/js/ex5.js
@@ -1,6 +1,14 @@
 function exo5(){
 
+let btn = document.getElementById('input1Ex5')
+let div = document.getElementById('div5')
 
+btn.addEventListener('click',()=>{
+    let image = document.createElement('img')
+    image.setAttribute('src','../../public/img/oeuf.png')
+    image.style.height = '75px'
+    div.appendChild(image)
+})
 
 }
 export {exo5};

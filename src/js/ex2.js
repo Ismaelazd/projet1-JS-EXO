4b7bc4d6acc69a49cb2a69384cabcf8f1398f64f
Ismael <ismaelazd@hotmail.com>
2019-12-10 21:47:02 +0100

    fin de section 4 html

--- src/js/ex2.js
+++ src/js/ex2.js
@@ -1,13 +1,6 @@
 function exo2(){
 
-let h2 = document.getElementById('h2ex2')
-let input = document.querySelector('.inputEx2')
-let btn = document.querySelector('.btnEx2')
 
-btn.addEventListener('click', ()=>{
-    h2.innerText = input.value
-    input.value=""
-})
 
 }
 export {exo2};
